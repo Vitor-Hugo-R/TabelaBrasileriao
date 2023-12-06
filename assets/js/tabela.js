afde4ecf36978fba2ef4834bc5176a4f0f924c9c
Vitor-Hugo-R <vi50_99@hotmail.com>
2023-12-05 21:54:43 -0300

    atualizado

--- assets/js/tabela.js
+++ assets/js/tabela.js
@@ -1,9 +1,23 @@
-function createGame(time1, hora, time2){
+function createGame(time1, hora, time2) {
     return `	
     <li>
-    <img src="assets/img/icont-${time1}.png" alt="Bandeira do ${time1}">
+    <img src="../assets/img/icon-${time1}.png" alt="Bandeira do ${time1}"/>
     <strong>${hora}</strong>
-    <img src="assets/img/icont-${time2}.png" alt="Bandeira do ${time2}">
+    <img src="../assets/img/icon-${time2}.png" alt="Bandeira do ${time2}"/>
     </li>
     `
 }
+let delay = -0.4;
+function createCard(data, dia, jogos) {
+    delay = delay + 0.4;
+    return `
+    <div class="card" style="animation-delay: ${delay}s">
+        <h2>${data} <span>${dia}</span></h2>
+        <ul>
+            ${jogos}
+        </ul>
+    </div>
+    `
+}
+document.querySelector("#cards").innerHTML =
+    createCard("06/12", "Quata-feira", createGame("santos", "21:30", "fortaleza")) 
